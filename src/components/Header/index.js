7fb283e3e4ca5c06fb439bba698bfbd2cb709d6a
Binh Ngo <bnngo97@gmail.com>
2023-02-28 21:52:32 -0800

    component folders and associated js and css files

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'style.css'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
---
+++ src/components/Signup/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'style.css'
+
+const Signup = () => {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
