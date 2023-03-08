5144ee2bb9bcb53bb8e2b552ebecdbd207134b7e
Binh Ngo <bnngo97@gmail.com>
2023-03-08 10:51:07 -0800

    starting on profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import "./style.css"
+import Hero from "../../components/Hero/index"
+import heroImg from "../Stores/assets/heroImg.jpg"
+import userImg from "../Stores/assets/userImg.jpg"
 
 function Profile() {
+  const storeObj =
+{
+  name:"Mcdonalds",
+  phoneNumber:"8888888",
+  email:"mcdonalds@email.com",
+  address:"123 Mcdonalds St",
+  tags: ["vegan", "spicy", "asian"],
+  ratings: ["5"],
+  heroImg: 
+    {
+      src: heroImg,
+      alt: "hero img",
+    }
+  ,
+  userImg: 
+    {
+      src:  userImg,
+      alt: 'user img'
+    }
+  
+}
   return (
-    <div>Profile</div>
+    <div> 
+    <Hero 
+      name={storeObj.name}
+      ratings={storeObj.ratings}
+      address={storeObj.address}
+      phoneNumber={storeObj.phoneNumber}
+      tags={[storeObj.tags]}
+      heroImg={storeObj.heroImg.src}
+      heroImgAlt={storeObj.heroImg.alt}
+      userImg={storeObj.userImg.src}
+      userImgAlt={storeObj.userImg.alt}
+    />
+  </div>
   )
 }
 
