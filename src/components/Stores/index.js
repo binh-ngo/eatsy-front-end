d9d26d45e0d041750cf0d0ed46050d061fef09ec
Bram Gibson <Bram.gibson@hotmail.com>
2023-03-07 18:09:17 -0800

    beginning of card functionality

--- src/components/Stores/index.js
+++ src/components/Stores/index.js
@@ -1,10 +1,47 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import ItemCards from "../ItemCards/index";
+import "./style.css";
+import taco from "./taco.jpg";
 
 const Stores = () => {
-  return (
-    <div>Stores</div>
-  )
-}
+  const cardArray = [
+    {
+      name: "taco",
+      id:"223",
+      allergens: ["Fish", "Dairy"],
+      description: "Fish Taco with all the fixins",
+      img: taco,
+    },
+    {
+      name: "taco",
+      id:"224",
+      allergens: ["Fish", "Dairy"],
+      description: "Fish Taco with all the fixins",
+      img: taco,
+    },
+  ];
 
-export default Stores
+  return (
+    <div>
+      <div>Stores</div>
+
+      {/* Brams Bottom ;) */}
+      <div>MENU</div>
+      <div id="itemCardsContainer">
+        <div id="bottomCardHalf" className="row">
+          {cardArray.map((item) => (
+            <ItemCards
+            key={item.id}
+              name={item.name}
+              allergens={[item.allergens]}
+              description={item.description}
+              img={item.img}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stores;
