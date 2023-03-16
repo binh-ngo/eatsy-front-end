0618a889bdf512cc123e2d21260c5c672a690dfb
agtTwilight <lukasmacmillen@gmail.com>
2023-03-15 20:48:23 -0700

    added tags update to the stores

--- src/pages/Stores/index.js
+++ src/pages/Stores/index.js
@@ -1,96 +1,80 @@
 import React from "react";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "./style.css";
-import Hero from "../../components/Hero/index";
-import heroImg from "./assets/heroImg.jpg";
-import userImg from "./assets/userImg.jpg";
 import ItemCards from "../../components/ItemCards/index";
 import StoreFrontDivider from "../../components/StoreFrontDivider/index";
 import { Container, Row, Button } from "react-bootstrap";
 import SendMessageModal from "../../components/SendMessageModal";
 import { useState, useEffect } from "react";
 import API from "../../utils/api"
+import demoGuy from "../../utils/assests/eatsy.png"
+import eatsy from "../../utils/assests/hero.png"
 
 function Stores() {
-  // get username param
-  const params = useParams();
-  
-  // fetch data
-  const [userData, setUserData] = useState([]);
-  const [companyData, setCompanyData] = useState([])
-  const [menuData, setMenuData] = useState([])
+        // get username param
+        const params = useParams();
 
-  useEffect(() => {
-    API.getSingleUser(params.username).then(res => {
-      setUserData(res)
-      setCompanyData(res.company)
-      setMenuData(res.company.menu)
-    })
-      .catch(err => console.log(err))
-  }, [])
+        // fetch data
+        const [userData, setUserData] = useState([]);
+        const [companyData, setCompanyData] = useState([])
+        const [menuData, setMenuData] = useState([])
 
-  // hard coded data
-  const storeObj =
-  {
-    name: "Mcdonalds",
-    phoneNumber: "8888888",
-    email: "mcdonalds@email.com",
-    address: "123 Mcdonalds St",
-    tags: ["vegan", "spicy", "asian"],
-    ratings: ["5"],
-    heroImg:
-    {
-      src: heroImg,
-      alt: "hero img",
-    }
-    ,
-    userImg:
-    {
-      src: userImg,
-      alt: 'user img'
-    }
-  }
+        useEffect(() => {
+                API.getSingleUser(params.username).then(res => {
+                        setUserData(res)
+                        setCompanyData(res.company)
+                        setMenuData(res.company.menu)
+                })
+                        .catch(err => console.log(err))
+        }, [])
 
-  return (
-    <Container className="stores">
-
-      <Row id="storeHero">
-        <Hero
-          // TODO remove rating, and phoneNumber here and in the hero itself
-          name={userData.username}
-          // tags={companyData.tags}
-          heroImg={storeObj.heroImg.src}
-          heroImgAlt={storeObj.heroImg.alt}
-          userImg={storeObj.userImg.src}
-          userImgAlt={storeObj.userImg.alt}
-          />
-          {/* TODO edit when JWT token is accessable */}
-          <SendMessageModal 
-          userTo = {params.username}
-          userFrom = {localStorage.getItem("username")}
-          />
-      </Row>
-      <Row className='divider'>
-        <StoreFrontDivider
-        />
-      </Row>
-      {/* Brams Bottom ;) */}
-      <Container id="itemCardsContainer">
-        <Row id="bottomCardHalf">
-          {menuData.map((item) => (
-            // TODO remove ingredients, ADD allergens to db
-            <ItemCards
-              key={item.id}
-              name={item.name}
-              allergens={[item.allergens]}
-              description={item.description}
-              src={item.src}
-            />
-          ))}
-        </Row>
-      </Container>
-    </Container>
-  );
+        return (
+                <section id="profileContainer" className="wrapper">
+                        <section id="heroContainer">
+                                <img src={eatsy} id="eatsyHero"></img>
+                                <img src={userData.src || demoGuy} id="userImg"></img>
+                                <div id="heroText">
+                                        <h1 id="heroName">Meet: {userData.username}</h1>
+                                        <p id="heroDescription">{companyData.description}</p>
+                                        <ul id="heroTags">
+                                        <li><u>TAGS</u></li>
+                                                                {companyData.tags?.length
+                                                                ?(companyData.tags.map(tag => (
+                                                                        <li>{tag}</li>
+                                                                ))): <li>N/A</li>
+                                                        }
+                                        </ul>
+                                </div>
+                                <section id="profileBtns">
+                                        <SendMessageModal
+                                                userTo={params.username}
+                                                userFrom={localStorage.getItem("username")}
+                                                name="Send Message"
+                                        />
+                                </section>
+                        </section>
+                        <StoreFrontDivider/>
+                        <section>
+                                <Container id="itemCardsContainer">
+                                        <Row id="bottomCardHalf">
+                                                {menuData?.length
+                                                        ? (
+                                                                menuData.map(item => (
+                                                                        <ItemCards
+                                                                                id={item._id}
+                                                                                name={item.name}
+                                                                                allergens={[item.allergens]}
+                                                                                description={item.description}
+                                                                                src={item.src}
+                                                                        />
+                                                                ))
+                                                        ) : <p>No menu to display</p>
+                                                }
+                                        </Row>
+                                </Container>
+                        </section>
+                </section>
+        );
 };
 
 export default Stores;
---
+++ src/pages/depricatedStores/index.js
@@ -0,0 +1,96 @@
+import React from "react";
+import {useParams} from "react-router-dom";
+import "./style.css";
+import Hero from "../../components/Hero/index";
+import heroImg from "./assets/heroImg.jpg";
+import userImg from "./assets/userImg.jpg";
+import ItemCards from "../../components/ItemCards/index";
+import StoreFrontDivider from "../../components/StoreFrontDivider/index";
+import { Container, Row, Button } from "react-bootstrap";
+import SendMessageModal from "../../components/SendMessageModal";
+import { useState, useEffect } from "react";
+import API from "../../utils/api"
+
+function Stores() {
+  // get username param
+  const params = useParams();
+  
+  // fetch data
+  const [userData, setUserData] = useState([]);
+  const [companyData, setCompanyData] = useState([])
+  const [menuData, setMenuData] = useState([])
+
+  useEffect(() => {
+    API.getSingleUser(params.username).then(res => {
+      setUserData(res)
+      setCompanyData(res.company)
+      setMenuData(res.company.menu)
+    })
+      .catch(err => console.log(err))
+  }, [])
+
+  // hard coded data
+  const storeObj =
+  {
+    name: "Mcdonalds",
+    phoneNumber: "8888888",
+    email: "mcdonalds@email.com",
+    address: "123 Mcdonalds St",
+    tags: ["vegan", "spicy", "asian"],
+    ratings: ["5"],
+    heroImg:
+    {
+      src: heroImg,
+      alt: "hero img",
+    }
+    ,
+    userImg:
+    {
+      src: userImg,
+      alt: 'user img'
+    }
+  }
+
+  return (
+    <Container className="stores">
+
+      <Row id="storeHero">
+        <Hero
+          // TODO remove rating, and phoneNumber here and in the hero itself
+          name={userData.username}
+          // tags={companyData.tags}
+          heroImg={storeObj.heroImg.src}
+          heroImgAlt={storeObj.heroImg.alt}
+          userImg={storeObj.userImg.src}
+          userImgAlt={storeObj.userImg.alt}
+          />
+          {/* TODO edit when JWT token is accessable */}
+          <SendMessageModal 
+          userTo = {params.username}
+          userFrom = {localStorage.getItem("username")}
+          />
+      </Row>
+      <Row className='divider'>
+        <StoreFrontDivider
+        />
+      </Row>
+      {/* Brams Bottom ;) */}
+      <Container id="itemCardsContainer">
+        <Row id="bottomCardHalf">
+          {menuData.map((item) => (
+            // TODO remove ingredients, ADD allergens to db
+            <ItemCards
+              key={item.id}
+              name={item.name}
+              allergens={[item.allergens]}
+              description={item.description}
+              src={item.src}
+            />
+          ))}
+        </Row>
+      </Container>
+    </Container>
+  );
+};
+
+export default Stores;
